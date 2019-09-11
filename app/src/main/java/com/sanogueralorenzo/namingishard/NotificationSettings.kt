5fdc335109906c1b4e409e6b509eb14f854e0455
Niels Evenblij <Niels.e@live.nl>
2019-09-11 13:45:24 +0200

    Revert "notificationhub"
    This reverts commit 7824e48a16adf2b0346a4a21ecfa45190cb3047d.

--- app/src/main/java/com/sanogueralorenzo/namingishard/NotificationSettings.kt
+++
@@ -1,6 +0,0 @@
-package com.sanogueralorenzo.namingishard
-
-object NotificationSettings {
-    var HubName = "<Your HubName>"
-    var HubListenConnectionString = "<Enter your DefaultListenSharedAccessSignature connection string>"
-}
